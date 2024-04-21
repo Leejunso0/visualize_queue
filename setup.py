422b1e1fba818aca7ee844eb66cc86fd2310e4dc
2juns <126080748+2juns@users.noreply.github.com>
2024-04-21 17:26:54 +0900

    update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 setuptools.setup(
     name = 'visualize_queue',
-    version= '0.0.1.1',
+    version= '0.0.1.2',
     long_description = long_description,
     long_description_content_type='text/markdown',
     description='visualize queue with matplotlib',
