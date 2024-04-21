03a909ca6c7fee0f449af68f4924f026c84ebdd6
2juns <126080748+2juns@users.noreply.github.com>
2024-04-21 16:43:58 +0900

    1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     name = 'visualize_queue',
     version= '0.0.1',
     description='visualize queue with matplotlib',
-    autor = 'Leejunsoo',
+    autor = 'junsoo',
     url = 'https://github.com/Leejunso0/visualize_queue',
     download_url= 'https://github.com/Leejunso0/visualize_queue',
     requires=[
